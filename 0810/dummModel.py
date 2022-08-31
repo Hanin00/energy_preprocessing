8d02320de8d17fca4773579f9fd17d8861ae763a
Hanin00 <haeun5419@a.ut.ac.kr>
2022-08-31 17:34:16 +0900

    :construction: before denomalization

--- 0810/dummModel.py
+++ 0810/dummModel.py
@@ -21,10 +21,10 @@ import tensorflow as tf
 from torch.utils.data import TensorDataset  # 텐서데이터셋
 from torch.utils.data import DataLoader  # 데이터로더
 from statsmodels.tsa.arima_model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 import statsmodels.api as sm
 
-#https://doheon.github.io/%EC%BD%94%EB%93%9C%EA%B5%AC%ED%98%84/time-series/ci-3.lstm-post/
-#todo model 위에거 써서 해보기
+
 
 #1-1. LSTM 일단위 + FC, 데이터 형태 확인
     #1-1-1. FC 할 때 weight 를 한 건지..? 걍 결과값 O
@@ -35,12 +35,15 @@ import statsmodels.api as sm
 #3-2. 기존 방법에서 Predict 한 방법 확인
 #todo 4-1. 모듈화
 #early stopping 코드 넣어야 함
-
+#todo train한 모델 저장 및 불러와 test 할 수 있도록 코드 추가
 
 #todo predict 값에 normalize 해서 plot으로 비교
 #ARIMA predict reversr 후 lstm 결과값과 concat 하면 됨
 
 # todo 지금 하루 데이터만 뽑는게 맞는지에 대해서도 봐야함
+
+# todo 일단 predict 코드를 만들어 보자.. 성능은 나중에.. 글고 아무리 생각해도 지금 normalize가 문제인 것 같은데.. 함 봐봅시다..
+# model 저장을 하고 predict를 하는지, 아니면 걍 값만 변경을 하는지, 값을 어떻게 주는지!
 
 
 #dataP = 0.7
@@ -71,12 +74,11 @@ def datasetXsml(df, seq_length ):
     train_set = df[0:train_size]
     trainDf = pd.DataFrame(train_set, columns=["power_value"])
     trainDf["power_value"] = scaler.fit_transform(trainDf["power_value"].values.reshape(-1, 1))
-    trainDf = trainDf["power_value"]
+    #trainDf = trainDf["power_value"]
     trainDf = trainDf[::-1]
 
     test_set = df[train_size - seq_length:]
     train_set = df[0:train_size]
-
     test_set = trainDf[train_size - seq_length:]
 
 
@@ -95,7 +97,57 @@ def datasetXsml(df, seq_length ):
     trainDataset = TensorDataset(trainX_tensor, trainY_tensor)
     testDataset = TensorDataset(testX_tensor, testY_tensor)
 
-    return trainX_tensor, trainY_tensor, testX_tensor, testY_tensor, trainDataset, testDataset
+    train_set = train_set[::-1]
+    test_set = test_set[::-1]
+
+    return train_set,test_set, trainX_tensor, trainY_tensor, testX_tensor, testY_tensor, trainDataset, testDataset
+
+
+
+#testS,E : test data를 나누는 구간
+#train, test set으로 Df를 나눔. normalized를 함(MinMax Scaler)
+#todo read me에 추가
+# data ag -> test Xm, Xl 사이즈가 24, 3개로 너무 적기 때문에 test 데이터의 경우 Xs 길이까지 맞춰서
+def datasetXsml2(df, seq_length, trainS, trainE, testS ):
+    #todo 날짜로 인덱싱
+    scaler = MinMaxScaler()
+    df["power_value"] = scaler.fit_transform(df["power_value"].values.reshape(-1, 1))
+    df = df["power_value"]
+    df = df[::-1]
+
+    train_set = df.loc[trainS:trainE]
+    trainDf = pd.DataFrame(train_set, columns=["power_value"])
+    trainDf["power_value"] = scaler.fit_transform(trainDf["power_value"].values.reshape(-1, 1))
+    trainDf = trainDf["power_value"]
+    test_set = df.loc[testS:]
+
+    testDf = pd.DataFrame(test_set, columns=["power_value"])
+    testDf["power_value"] = scaler.fit_transform(testDf["power_value"]  .values.reshape(-1, 1))
+    testDf = testDf["power_value"]
+
+    trainX, trainY = build_dataset(np.array(trainDf), seq_length)
+    testX, testY = build_dataset(np.array(testDf), seq_length)
+
+
+    # 텐서로 변환
+    trainX_tensor = torch.FloatTensor(trainX)
+    trainY_tensor = torch.FloatTensor(trainY)
+    testX_tensor = torch.FloatTensor(testX)
+    testY_tensor = torch.FloatTensor(testY)
+
+    # 텐서 형태로 데이터 정의
+
+    trainDataset = TensorDataset(trainX_tensor, trainY_tensor)
+    testDataset = TensorDataset(testX_tensor, testY_tensor)
+
+
+    # train_set = trainDf
+    # test_set = testDf
+    train_set = train_set[::-1]
+    test_set = test_set[::-1]
+
+    return train_set,test_set, trainX_tensor, trainY_tensor, testX_tensor, testY_tensor, trainDataset, testDataset
+
 
 
 # Here we define our model as a class
@@ -113,6 +165,7 @@ class LSTMModel(nn.Module):
 
         # layer
         self.activate_func = tf.nn.relu
+
         self.lstmXm = nn.LSTM(inputXm_dim, hidden_dim, num_layers, batch_first=True)
         self.lstmXl = nn.LSTM(inputXl_dim, hidden_dim, num_layers, batch_first=True)
         #self.fcToLSTM_layer = fcToLSTM_layer(self, x, hidden_dim, num_layers, whatIs)  # fc - lstm - fc fore
@@ -166,87 +219,65 @@ class LSTMModel(nn.Module):
 
         return out
 
+
+
 pd.set_option('display.max_columns', None)
 # 데이터 불러오기
-# df10T = pd.read_csv('../data/dailyPred/target102_10T_diff.csv', parse_dates=['updated'], encoding='utf-8', )
-# df10T.set_index('updated', inplace=True)
-# df1D = pd.read_csv('../data/dailyPred/target102_1D_diff.csv', parse_dates=['updated'], encoding='utf-8', )
-# df1D.set_index('updated', inplace=True)
-
-
-
-#todo 지금 데이터는 10분 씩 되어 있음. LSTM에 넣기 위해서 하루 단위로 바꿔주는 모듈 만들어야 함
-#todo 하루 단위로 일단 결과값을 받고 그걸 모듈화 해 봅시다. 근데 이거 별로 좋은 습관은 아닌 듯. 이렇게 하면 일을 두 번 해야하니까 습관 개선이 필요해 보임
-
 #df1D = pd.read_csv('../data/total_pv.csv', parse_dates=['updated'], encoding='utf-8', )
 df1D = pd.read_csv('../data/total_pv_0831.csv', parse_dates=['updated'], encoding='utf-8', )
-dateColumn = 'updated'
-df1D.set_index(dateColumn, inplace=True)
-resultDf = df1D.resample('D').last()
-
-print(resultDf.loc['2019-01-01' : '2019-01-13'].head(10))
-print(resultDf.loc['2019-01-13' : '2019-01-13'].tail(10))
-
-sys.exit()
-
-
-
-
 # pd1D = tp.resample('1D').last() #하루 단위 resampling
 dateColumn = 'updated'
 freq = 'D'
 
 
-
 def slicerFreq(df, freq, dateColumn) :
     df.set_index(dateColumn, inplace=True)
     resultDf = df.resample(freq).last()
+    df_intp_linear = resultDf.interpolate()
+    resultDf["power_value"] = df_intp_linear[["power_value"]]
     return resultDf
 
-
-resultDf = slicerFreq(df1D, freq, dateColumn)
-df_intp_linear = resultDf.interpolate()
-resultDf["power_value"] = df_intp_linear[["power_value"]]
-
-
-# print(resultDf[resultDf["power_value"].isnull()])
-# print(resultDf.isnull().sum())
-
-
-
-df1D = resultDf
-df1D_arima = df1D
-#arima는 datasetXsml을 사용하지 않아서 따로 정규화를 해 줬었는데 이렇게 하지 말고
-#todo datasetXml의 결과값을 쓰도록 변경
-scaler = MinMaxScaler()
-df1D_arima["power_value"] = scaler.fit_transform(df1D_arima["power_value"].values.reshape(-1, 1))
-df1D_arima = df1D_arima["power_value"]
-df1D_arima = df1D_arima[::-1]
-
-
-
-#train_size = int(len(df1D_arima) * 0.7)
-train_size = int(len(df1D_arima) * dataP)
-train_set_AR = df1D_arima[0:train_size]
-train_set_AR = train_set_AR[::-1]  # arima에 쓸 쑤 있는 데이터로 만들기 위해 reverse
-#test_set_AR = df1D_arima[train_size - seq_length:]
-
-model  = sm.tsa.arima.ARIMA(train_set_AR, order = (2,1,2),freq = 'D',missing = "drop")
-model_fit = model.fit()
-
-preds_arima = model_fit.predict()
-preds_arima = torch.FloatTensor(preds_arima.tolist())
-preds_arima = torch.flip(preds_arima, [0])
-preds_arima = torch.unsqueeze(preds_arima, 1)
+resultDf = slicerFreq(df1D, freq, dateColumn) #일 단위 데이터로 변환 및 결측치 선형 보간
 
 #trainXs_tensor, trainYs_tensor, testXs_tensor, testYs_tensor, trainDatasetXs, testDatasetXs = datasetXsml(df10T, 1)
-trainXs_tensor, trainYs_tensor, testXs_tensor, testYs_tensor, trainDatasetXs, testDatasetXs = datasetXsml(df1D, 1)
-trainXm_tensor, trainYm_tensor, testXm_tensor, testYm_tensor, trainDatasetXm, testDatasetXm = datasetXsml(df1D, 7)
-trainXl_tensor, trainYl_tensor, testXl_tensor, testYl_tensor, trainDatasetXl, testDatasetXl = datasetXsml(df1D, 28)
+# ARTrainset,ARTestset, trainXs_tensor, trainYs_tensor, testXs_tensor, testYs_tensor, trainDatasetXs, testDatasetXs = datasetXsml(resultDf, 1)
+# _, _, trainXm_tensor, trainYm_tensor, testXm_tensor, testYm_tensor, trainDatasetXm, testDatasetXm = datasetXsml(resultDf, 7)
+# _, _, trainXl_tensor, trainYl_tensor, testXl_tensor, testYl_tensor, trainDatasetXl, testDatasetXl = datasetXsml(resultDf, 28)
 
-# #ARIMA
-#trainXs2_tensor, trainYs2_tensor, testXs2_tensor, testYs2_tensor, trainDatasetXs2, testDatasetXs2 = datasetXsml(df10T, 144)
-# datasetXs = trainDatasetXs
+trainS = '2019-01-01'
+trainE = '2021-07-31'
+testS = '2021-08-01'
+
+
+ARTrainset,ARTestset, trainXs_tensor, trainYs_tensor, testXs_tensor, testYs_tensor, trainDatasetXs, testDatasetXs = datasetXsml2(resultDf, 1,  trainS, trainE, testS )
+_, _, trainXm_tensor, trainYm_tensor, testXm_tensor, testYm_tensor, trainDatasetXm, testDatasetXm = datasetXsml2(resultDf, 7, trainS, trainE, testS )
+_, _, trainXl_tensor, trainYl_tensor, testXl_tensor, testYl_tensor, trainDatasetXl, testDatasetXl = datasetXsml2(resultDf, 28, trainS, trainE, testS )
+
+
+# todo read me update -> test Dataset
+def testDataTrimming(testXs_tensor, testXm_tensor, testXl_tensor) :
+    tXslen = len(testXs_tensor)
+    lastTXm = testXm_tensor[-1]
+    lastTXl = testXl_tensor[-1]
+
+    dumTXm = list(testXm_tensor)
+    for i in range(tXslen):
+        if len(dumTXm) == tXslen :
+            break
+        dumTXm.append(lastTXm)
+
+    dumTXl = list(testXl_tensor)
+    for i in range(tXslen):
+        if len(dumTXl) == tXslen:
+            break
+        dumTXl.append(lastTXl)
+
+    dumTXm = torch.stack(dumTXm, dim=0)
+    dumTXl = torch.stack(dumTXl, dim=0)
+
+    return dumTXm, dumTXl
+
+testXm_tensor, testXl_tensor  = testDataTrimming(testXs_tensor, testXm_tensor, testXl_tensor)
 
 # Build model
 input_dim = 1
@@ -259,10 +290,10 @@ inputXl_dim = trainXl_tensor.size()[1]
 #build model
 model = LSTMModel(inputXm_dim, inputXl_dim, hidden_dim, num_layers, output_dim)
 loss_fn = torch.nn.MSELoss()
-optimiser = torch.optim.Adam(model.parameters(), lr=0.01)
+#optimiser = torch.optim.Adam(model.parameters(), lr=0.01)
+optimiser = torch.optim.Adam(model.parameters(), lr=0.1)
 
 # Train model
-#####################
 num_epochs = 200
 hist = np.zeros(num_epochs)
 
@@ -286,8 +317,53 @@ class EarlyStopping:
     def is_stop(self):
         return self.patience >= self.patience_limit
 
-#early_stop = EarlyStopping(patience=10)
+#early_stop = EarlyStopping(patience=30)
+'''
+    Auto Regressive - Arima model
+    동일한 데이터(2019-01 ~ 2021-07)을 train 데이터로 사용
+'''
 
+
+# ARTrainset = ARTrainset.loc['2019-01-01' : '2021-08-31']
+# arima_model  = sm.tsa.arima.ARIMA(ARTrainset, order = (2,1,2),freq = 'D',missing = "drop")
+#ARTrainset = ARTrainset.loc['2019-01-01' : '2021-08-31']
+
+arima_model  = sm.tsa.arima.ARIMA(ARTrainset, order = (2,1,2),freq = 'D',missing = "drop")
+model_fit = arima_model.fit()
+
+# preds_arima = model_fit.predict()
+# preds_arima = torch.FloatTensor(preds_arima.tolist())
+# preds_arima = torch.flip(preds_arima, [0])
+# preds_arima = torch.unsqueeze(preds_arima, 1)
+
+#todo 원상 복구
+pred = model_fit.get_prediction(start=pd.to_datetime('2019-01-01'), end = pd.to_datetime('2021-07-31'), )
+#preds_arima = model_fit.predict()
+
+
+preds_arima = pred.predicted_mean
+#preds_arima = torch.FloatTensor(preds_arima.tolist())
+preds_arima = torch.FloatTensor(preds_arima)
+preds_arima = torch.flip(preds_arima, [0])
+preds_arima = torch.unsqueeze(preds_arima, 1)
+
+
+# #todo 최근 한 달에 대한 arima 예측
+# pred = model_fit.get_prediction(start=pd.to_datetime('2021-08-01'), end = pd.to_datetime('2021-08-31'), )
+#
+# pred_ci = pred.conf_int()
+# ## Plot in-sample-prediction
+# ax = ARTrainset['2019':].plot(label='Observed')
+# pred.predicted_mean.plot(ax=ax, label="one-step Ahead Prediction", alpha = .5, )
+#
+# ax.set_xlabel('Date')
+# ax.set_ylabel('PowerValue')
+# plt.legend(loc = 'upper left')
+# plt.show()
+#
+# sys.exit()
+
+print("LSTM start " )
 for t in range(num_epochs):
     y_train_pred = model(trainXs_tensor, trainXm_tensor, trainXl_tensor, hidden_dim, num_layers, output_dim)
     #todo ARIMA랑 concat 후, Xs의 Y 값과 일치하는지 확인. Y_train_pred 도 그러면 dimension 같아야겠지 ~
@@ -296,77 +372,112 @@ for t in range(num_epochs):
     preds_arima = preds_arima.split(len(y_train_pred), dim=0)[0]
     y_train_pred += preds_arima
     y_train_pred = torch.flip(y_train_pred, [0])
-
-
-    # #todo y_train_pred 값에 대해 normalize? 안해도 잘 나와야하는거 아닌가?
-    # # scaler = MinMaxScaler()
-    # df1D_arima["power_value"] = scaler.fit_transform(df1D_arima["power_value"].values.reshape(-1, 1))
-    # df1D_arima = df1D_arima["power_value"]
-    # df1D_arima = df1D_arima[::-1]
-
     loss = loss_fn(y_train_pred, y_train)
-#todo early stopping
+#early stopping
     # early_stop.step(loss.item())
-    # if early_stop.is_stop():
+     # if early_stop.is_stop():
     #     break
-
     if t % 10 == 0 and t != 0:
         print("Epoch ", t, "MSE: ", loss.item())
+
     hist[t] = loss.item()
-    # Zero out  "gradient, else they will accumulate between epochs
+    # Zero out  "gradient, else t hey will accumulate between epochs
     optimiser.zero_grad()
     # Backward pass
     loss.backward()
     # Update parameters
     optimiser.step()
 
-## Train Fitting1
+#todo model 저장
+torch.save(model, './model/model.pt')
+torch.save(model_fit, './model/arima_model_fit.pt')
+
+## Train Fitting
 plt.title('Train')
-plt.plot(y_train_pred.detach().numpy(), label="Preds")
-plt.plot(y_train.detach().numpy(), label="Real")
+plt.plot(y_train_pred.detach().numpy(), label="Preds", alpha = .7)
+plt.plot(y_train.detach().numpy(), label="Real", alpha = .7)
+#plt.xticks(ARTrainset.index)
 plt.legend()
 plt.show()
 
-#np.shape(y_train_pred)
 
+#np.shape(y_train_pred)
 # make predictions
 '''
-Test Dataset 사용
     최근 한달(2021-08)월에 대해 예측
     08월은 Y값으로 주어짐
 '''
+#todo model 불러오기
+#model = torch.load('./model/model.pt')
+model_fit = torch.load('./model/arima_model_fit.pt')
+
+def testDataTrimming(testXs_tensor, testXm_tensor, testXl_tensor) :
+    tXslen = len(testXs_tensor)
+    lastTXm = testXm_tensor[-1]
+    lastTXl = testXl_tensor[-1]
+
+    dumTXm = list(testXm_tensor)
+    print(dumTXm)
+    dumTXl = []
+
+    return testXs_tensor, testXm_tensor, testXl_tensor
+testXs_tensor, testXm_tensor, testXl_tensor  = testDataTrimming(testXs_tensor, testXm_tensor, testXl_tensor)
+
+
+
 
 y_test_pred = model(testXs_tensor, testXm_tensor, testXl_tensor, hidden_dim, num_layers, output_dim)
-
-print("y_test_pred.size() : ", y_test_pred.size())
-
-
-
-
-arima_test_pred = model_fit.get_prediction(start=pd.to_datetime('2021-08-01'), end = pd.to_datetime('2021-08-31'), )
-y_test_pred += arima_test_pred
-y_train_pred = torch.flip(y_train_pred, [0])
-
 y_test = torch.flip(testYs_tensor, [0])  # tensor reverse
-y_test = y_test.split(len(y_train_pred), dim=0)[0]
+y_test = y_test.split(len(y_test_pred), dim=0)[0]
 
+#todo 최근 한 달에 대한 arima 예측
+arima_test_pred = model_fit.get_prediction(start=pd.to_datetime('2021-08-01'), end = pd.to_datetime('2021-08-31'), )
+arima_test_pred = arima_test_pred.predicted_mean # : 31
+arima_test_pred = torch.FloatTensor(arima_test_pred)
+arima_test_pred = torch.flip(arima_test_pred, [0])
+arima_test_pred = torch.unsqueeze(arima_test_pred, 1)
+arima_test_pred = arima_test_pred.split(len(y_test_pred), dim=0)[0]
+y_test_pred += arima_test_pred
+y_test_pred = torch.flip(y_test_pred, [0])
 
+# # arima_test_pred = torch.FloatTensor(arima_test_pred).split(len(y_test_pred), dim=0)[0]
+# # arima_test_pred = torch.FloatTensor(arima_test_pred.tolist())
+# # arima_test_pred = torch.flip(arima_test_pred, [0])
+# # arima_test_pred = torch.unsqueeze(arima_test_pred, 1)
+#
+# y_test_pred += arima_test_pred
+# y_test_pred = torch.flip(y_test_pred, [0])
 
-print("testYs_tensor : ",testYs_tensor)
-print("testYs_tensor.shape() : ",testYs_tensor.shape())
-
-sys.exit()
-
-
-
-loss = loss_fn(y_train_pred, y_train)
+loss = loss_fn(y_test_pred, y_test)
 print("test loss : ", loss.item())
+
 
 plt.title('Test')
 plt.plot(y_test_pred.detach().numpy(), label="Preds")
 plt.plot(y_test.detach().numpy(), label="Real")
 plt.legend()
 plt.show()
+
+sys.exit()
+
+''' 
+    학습과 테스트 이어서 예측..
+    -> 테스트 결과값이 한 달에 대한 예측같지가 않음
+    예측 후 
+'''
+
+
+
+plt.title('Train+Test')
+
+pred = y_train_pred.detach().numpy() + y_test_pred.detach().numpy()
+real = y_train.detach().numpy() + y_test.detach().numpy()
+
+plt.plot(pred, label="Preds")
+plt.plot(real, label="Real")
+plt.legend()
+plt.show()
+
 
 
 sys.exit()
