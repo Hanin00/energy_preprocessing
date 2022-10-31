09dee3d3a0a643c4d6c2fa35b47f6784510da243
Hanin00 <haeun5419@a.ut.ac.kr>
2022-10-31 13:31:50 +0900

    :sparkle: ARIMA

--- Final/Energy_pv/Train.py
+++ Final/Energy_pv/Train.py
@@ -1,10 +1,12 @@
-# ref) https://wikidocs.net/50949
 import sys
 import argparse
 import pandas as pd
+import matplotlib
+import matplotlib.pyplot as plt
 import statsmodels.api as sm
-from .config import parse_encoder
+from .config import arima_parse
 from statsmodels.tsa.arima.model import ARIMA
+from matplotlib import font_manager, rc
 
 # 10T data -> 1D data resampling & 선형 보간
 def resampleFreq(args, df) :
@@ -26,24 +28,41 @@ def Train(args, resultDf) :
 def Predict(args, model) :
     # preds = model_fit.predict(1, 30, typ='levels')
     preds = model.predict(args.pred_s,args.pred_e, typ='levels')
+    with open(args.pred_result, 'w') as csv_file:
+        preds.to_csv(path_or_buf=csv_file)
+
     return preds
-def Visualize() :
+def Visualize(pred_arima_y, test_y, path) :
+    matplotlib.rcParams['font.family'] = 'Malgun Gothic'
+    matplotlib.rcParams['axes.unicode_minus'] = False
+
+    plt.figure(figsize=(15, 10))
+    # 모델이 예측한 가격 그래프
+    plt.plot(pred_arima_y, color='gold')
+    # 실제 가격 그래프
+    plt.plot(test_y, color='green')
+
+    plt.legend(['예측값', '실제값'])
+    plt.title("값 비교")
+    plt.show()
+    plt.savefig(path)
+
     return
 
 
 def main():
     parser = argparse.ArgumentParser(description='Embedding arguments')
-    # utils.parse_optimizer(parser)
-    parse_encoder(parser)
+    arima_parse(parser)
     args = parser.parse_args()
 
     pd.set_option('display.max_columns', None)
-
     df1D = pd.read_csv(args.path, parse_dates=[args.date_column], encoding='utf-8', )
 
     resultDf = resampleFreq(args, df1D)  # 일 단위 데이터로 변환 및 결측치 선형 보간
     model_fit = Train(args, resultDf)
     pred = Predict(args, model_fit)
+    Visualize(pred, resultDf[-31:], args.result_visualize)
+
     print(pred)
 
 
--- Final/Energy_pv/config.py
+++ Final/Energy_pv/config.py
@@ -1,13 +1,16 @@
 import argparse
 
-def parse_encoder(parser, arg_str=None):
+def arima_parse(parser, arg_str=None):
     enc_parser = parser.add_argument_group()
     enc_parser.set_defaults(order=(2, 1, 2),
                             freq='D',
-                            path = 'Final/Energy_pv/data/new_total_pv_0831.csv',
-                            pred_s = "2021-09-01",
-                            pred_e = "2021-09-30",
-                            model_save = 'Final/Energy_pv/model/ARIMA_1031_fit.pkl',
-                            date_column = 'updated')    # True
+                            path = 'Final/Energy_pv/data/power_value.csv',
+                            pred_s = "2021-08-01",
+                            pred_e = "2021-08-31",
+                            model_save = 'Final/Energy_pv/model/ARIMA_fit.pkl',
+                            date_column = 'updated',
+                            pred_result='Final/Energy_pv/output/arima_predict.csv',
+                            result_visualize='Final/Energy_pv/output/arima_predict.png',
+                            )    # True
 
     # return enc_parser.parse_args(arg_str)
